923bf73f05e7fd3bc6ad8542d179dc93bc161e70
Hryhorii-Shtanko <gregory.shtanko@yahoo.com>
2024-12-25 15:32:22 +0200

    add few small fixes, play with some bootstrap styles

--- js/index.js
+++ js/index.js
@@ -6,26 +6,40 @@ function init() {
     
   }
   
-  import EmblaCarousel from 'embla-carousel';
+  // import EmblaCarousel from 'embla-carousel';
 
-  document.addEventListener("DOMContentLoaded", () => {
-    // Знаходимо контейнер каруселі
-    const emblaNode = document.querySelector(".embla");
+  // document.addEventListener("DOMContentLoaded", () => {
+  //   // Знаходимо контейнер каруселі
+  //   const emblaNode = document.querySelector(".embla");
     
-    // Ініціалізуємо Embla
-    const embla = EmblaCarousel(emblaNode, {
-      loop: true, // Зациклення
-      draggable: true, // Перетягування мишкою/пальцем
-    });
+  //   // Ініціалізуємо Embla
+  //   const embla = EmblaCarousel(emblaNode, {
+  //     loop: true, // Зациклення
+  //     draggable: true, // Перетягування мишкою/пальцем
+  //   });
   
-    // Опціонально: додай кнопки для управління
-    const prevButton = document.querySelector(".embla__button--prev");
-    const nextButton = document.querySelector(".embla__button--next");
+  //   // Опціонально: додай кнопки для управління
+  //   const prevButton = document.querySelector(".embla__button--prev");
+  //   const nextButton = document.querySelector(".embla__button--next");
   
-    prevButton?.addEventListener("click", () => embla.scrollPrev());
-    nextButton?.addEventListener("click", () => embla.scrollNext());
-  });
-  
+  //   prevButton?.addEventListener("click", () => embla.scrollPrev());
+  //   nextButton?.addEventListener("click", () => embla.scrollNext());
+  // });
+  import EmblaCarousel from "embla-carousel";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const emblaNode = document.querySelector(".embla");
+  const options = { loop: false, align: "start", slidesToScroll: 1 };
+  const embla = EmblaCarousel(emblaNode, options);
+
+  // Додаємо обробники подій для кнопок "Next" і "Prev"
+  const prevBtn = document.querySelector(".embla__prev");
+  const nextBtn = document.querySelector(".embla__next");
+
+  prevBtn.addEventListener("click", embla.scrollPrev);
+  nextBtn.addEventListener("click", embla.scrollNext);
+});
+
 
   const totalPartials = document.querySelectorAll(
     '[hx-trigger="load"], [data-hx-trigger="load"]'
